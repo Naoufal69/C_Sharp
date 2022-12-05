ff951ac34054f34d5bf7f88d9380248b35b0d946
Naoufal69 <naoufal.ayache@ecole-isitech.fr>
2022-12-05 12:26:04 +0100

    TD5 v1

---
+++ TP5/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿class Student
+{
+    string name { get; set; }
+}
+static void ChangeValue(Student student)
+{
+    student.name = 'Nicolas';
+}
+
+Student Kevin = new Student();
+Kevin.name = "Kévin";
+
+ChangeValue(Kevin);
+
+Console.WriteLine(Kevin.name);
