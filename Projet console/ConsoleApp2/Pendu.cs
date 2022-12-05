c3a05d561110d0ae641724a2011b86d87a2fc0bc
Naoufal69 <naoufal.ayache@ecole-isitech.fr>
2022-12-05 15:11:13 +0100

    Projet console v2

--- Projet console/ConsoleApp2/Pendu.cs
+++ Projet console/ConsoleApp2/Pendu.cs
@@ -6,28 +6,71 @@ namespace MySpacePendu
     {
         public string word { get; set; }
         public int[] check { get; set; }
+        public bool game { get; set; }
+        public int errorNumber { get; set; }
         public Pendu()
         {
-            string[] dico = { "Bonjour", "Nicolas","Test","Feu","Meilleur","Constitution"};
+            string[] dico = { "Bonjour","Nicolas","Test","Feu","Meilleur","Constitution"};
+            this.game = true;
             var rand = new Random();
             int i = rand.Next(0, dico.Length);
-            this.word = dico[i];
+            this.word = dico[i].ToLower();
             this.check = new int[this.word.Length];
             for (int j = 0; j < this.check.Length; j++)
             {
                 this.check[j] = 0;
             }
+            this.errorNumber = 0;
+            printWordToGuess();
+        }
+
+        public void printWordToGuess()
+        {
+            for (int j = 0; j < this.check.Length; j++)
+            {
+                if (this.check[j] == 0)
+                    Console.Write("_");
+                else
+                    Console.Write(this.word[j]);
+            }
+            Console.WriteLine();
+        }
+
+        public int checkIfWinOrLoose()
+        {
+            if (this.errorNumber >= 10)
+            {
+                this.game = false;
+                Console.WriteLine("Vous avez perdu !");
+                return -1;
+            }  
+            int sum = 0;
+            for (int i = 0; i < this.check.Length; i++)
+                sum += this.check[i];
+            return sum;
         }
 
         public void checkChar(char ch)
         {
             int i = 0;
-            foreach (char c in word) {
-                if (c == ch)
+            bool isIn = false;
+            for (int j = 0; j < this.word.Length; j++)
+            {
+                if (ch == this.word[j])
                 {
-                    this.check[i]++;
+                    this.check[j]++;
+                    isIn = true;
                 }
             }
+
+            if (isIn)
+                Console.WriteLine($"La lettre {ch} est présent dans le mot");
+            else
+                this.errorNumber++;
+            int numberToCheck = checkIfWinOrLoose();
+            if (numberToCheck == this.check.Length && numberToCheck > 0)
+                this.game = false;
+            printWordToGuess();
         }
     }
 }
--- Projet console/ConsoleApp2/Program.cs
+++ Projet console/ConsoleApp2/Program.cs
@@ -1,3 +1,20 @@
 ﻿using MySpacePendu;
 
 Pendu pendu = new Pendu();
+string guess;
+char ch;
+Console.WriteLine(pendu.word);
+while (pendu.game == true)
+{
+    Console.Write("Donnez une lettre : ");
+    try
+    {
+        guess = Console.ReadLine();
+        ch = guess[0];
+        pendu.checkChar(ch);
+    }
+    catch (IndexOutOfRangeException)
+    {
+        Console.WriteLine("Mauvais input !! recommencez ! ");
+    }
+}
