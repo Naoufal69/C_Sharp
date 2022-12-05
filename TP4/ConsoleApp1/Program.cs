550e6d0a7ab25579a3b94e9025409ad477321566
Naoufal69 <naoufal.ayache@ecole-isitech.fr>
2022-12-05 12:16:45 +0100

    TD4 v1

---
+++ TP4/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿static void ChangeValue(int x)
+{
+    x = 10;
+    Console.Write("La valeur de x est : ");
+    Console.WriteLine(x);
+}
+
+int i = 100;
+Console.WriteLine(i); //100
+ChangeValue(i); //10
+Console.WriteLine(i); //10
