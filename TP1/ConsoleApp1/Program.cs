5cc8dc889dc7d32a4f868a6117650d21d3f47e0a
Naoufal69 <naoufal.ayache@ecole-isitech.fr>
2022-12-05 11:22:52 +0100

    TD2 v1

--- TP1/ConsoleApp1/Program.cs
+++ TP1/ConsoleApp1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string name = "Naoufal";
 int age = 22;
-Console.WriteLine("Hello "+ name + " tu a " + age + " ans");
+Console.WriteLine("Hello " + name + " tu a " + age + " ans");
 if (age > 18)
 {
     Console.WriteLine("Tu es majeur");
---
+++ TP2/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int x = 5;
+if (x > 23)
+    x -= 3;
+else if (x == 23)
+    x -= 2;
+else
+    x += 4; //La variable prendera donc +4 et sera a 9
+Console.WriteLine(x); // Compilé avec succès
