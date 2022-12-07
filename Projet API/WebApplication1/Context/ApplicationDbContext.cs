4f5ec05f7d42e05eda6d0b6071ec6370c09f620e
Naoufal69 <naoufal.ayache@ecole-isitech.fr>
2022-12-07 15:25:44 +0100

    Projet API - Migration on db failed

--- Projet API/WebApplication1/Context/ApplicationDbContext.cs
+++ Projet API/WebApplication1/Context/ApplicationDbContext.cs
@@ -1,9 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 public class ApplicationDbContext : DbContext
 {
-    protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-        optionBuilder.UseMySQL("server=127.0.0.1;Port=1234;database=api-dotnet;uid=root;pwd='';");
+        
     }
 
     public DbSet<Product> products { get; set; }
---
+++ Projet API/WebApplication1/Migrations/20221207141738_initialMigrations.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    public partial class initialMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Price = table.Column<double>(type: "double", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "products");
+        }
+    }
+}
